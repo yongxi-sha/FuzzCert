923b1e9dea4d7168ef2563d741b7ce1d775a1458
Yongxi <50694123+sneakyggg@users.noreply.github.com>
2025-09-18 21:43:25 -0600

    Update adapters.py

--- adapters/adapters.py
+++ adapters/adapters.py
@@ -2,10 +2,6 @@ from falsify_adapter import FalsifyAdapter
 from verapak_adapter import VerapakAdapter
 
 ADAPTERS = {
-"""
-    Add benchmark name here
-    format: "benchmark name": 
-"""
     "verapak": VerapakAdapter,
     "falsify": FalsifyAdapter,
 }
